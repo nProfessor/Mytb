af3648dc3ba0bb125947dd366201c20057163b2a
Tabota Oleg <info@mytb.ru>
2012-11-05 19:45:13 +0400

    fix

--- bitrix/components/mytb/baner.home/templates/.default/template.php
+++ bitrix/components/mytb/baner.home/templates/.default/template.php
@@ -2,7 +2,7 @@
 <div class="base">
 <blockquote>
     <p>Подпишись на акции любимых заведений!</p>
-    <small> У нас <span class="red"><?=$arResult["CLUB_COUN"]?></span> заведения, и <span class="red"><?=$arResult["CLUB_STOCK"]?></span> действующие акции.<br/></small>
+    <small> У нас <span class="red"><?=$arResult["CLUB_COUN"]?></span> <?=declOfNum($arResult["CLUB_COUN"],array("заведение","заведения","заведений"));?>, и <span class="red"><?=$arResult["CLUB_STOCK"]?></span> <?=declOfNum($arResult["CLUB_STOCK"],array("действующая акция","действующие акции","действующих акций"));?>.<br/></small>
 </blockquote>
 </div>
 <br/>
