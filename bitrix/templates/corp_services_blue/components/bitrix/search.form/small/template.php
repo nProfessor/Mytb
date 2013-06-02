f8ca1c80a287c95af426d8923092007655cf72cc
Tabota Oleg <info@mytb.ru>
2013-06-02 19:35:15 +0400

    baner map

--- bitrix/templates/corp_services_blue/components/bitrix/search.form/small/template.php
+++ bitrix/templates/corp_services_blue/components/bitrix/search.form/small/template.php
@@ -4,12 +4,12 @@
     <input id="appendedInputButtons" size="16" type="text" value="<?$arParams["REQWEST"]?>" placeholder="Текст для поиска..." name="q"><button class="button orange" style="width:124px" type="submit"><?=GetMessage("BSF_T_SEARCH_BUTTON");?></button>
     </form>
 </div>
+<?
+$url=$APPLICATION->GetCurUri();
 
-<!--	<div class="flat">-->
-<!---->
-<!--	<div id="search-button">-->
-<!--    <input type="submit" name="s" id="search-submit-button" value="--><?//=GetMessage("BSF_T_SEARCH_BUTTON");?><!--" onfocus="this.blur();">-->
-<!--</div>-->
-<!--<div class="search-box"><input type="text" name="q"></div>-->
-<!---->
-<!--</div>-->
+$url=preg_replace("#(/)$#i","",$url);
+
+if($url!="/page/map"):?>
+<a href="/page/map" class="baner_top_map" title="Все заведения с акциями и скидками на карте"><img src="/images/baners/baner_map_search.jpg"</a>
+<?endif;?>
+
